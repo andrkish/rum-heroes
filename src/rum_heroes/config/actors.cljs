d49a20b9679240eb191e2b036fa0d2ff90da717a
Andrew Kishchenko <psspectre@gmail.com>
2018-04-04 17:20:55 +0300

    more actor templates (and sprites)

--- src/rum_heroes/config/actors.cljs
+++ src/rum_heroes/config/actors.cljs
@@ -1,9 +1,19 @@
 (ns rum-heroes.config.actors)
 
-(defonce actors-good-pack [ "knight" "mage" ])
+(defonce actors-good-pack [ "knight" "mage" "archer" "swordman" "berserk" ])
 
-(defonce actors-template 
-  { :knight { :visual "actor-knight" 
-              :hpMax 15 }
-    :mage   { :visual "actor-mage" 
-              :hpMax 5 }})
+;; helper function
+;; todo: learn about macro? 
+(defn create-template 
+  [ key sprite hpMax ]
+  (hash-map 
+    key { :visual sprite 
+          :hpMax hpMax }))
+
+(def actors-template 
+  (into {} 
+    [(create-template :knight "actor-knight" 15)
+     (create-template :mage "actor-mage" 5)
+     (create-template :archer "actor-archer" 7)
+     (create-template :swordman "actor-swordman" 10)
+     (create-template :berserk "actor-berserk" 12)]))
