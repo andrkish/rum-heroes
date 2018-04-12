8d8e5ab7117e39aa19070cff5fa3b87af0e23fe6
Andrew Kishchenko <psspectre@gmail.com>
2018-04-13 00:48:12 +0300

    fix call before declaration

--- src/rum_heroes/config/actors.cljs
+++ src/rum_heroes/config/actors.cljs
@@ -15,20 +15,20 @@
           :attacks 1
           :hpMax hpMax }))
 
-;; get static template from actor entity
-(defn get-template [actor]
-  (get actors-template (keyword (get actor :template))))
-
 (def actors-template 
   (into {} 
-    [(create-template :knight "actor-knight" 15 3 1 3)
+    [(create-template :knight "actor-knight" 15 4 1 3)
      (create-template :mage "actor-mage" 5 3 2 1)
      (create-template :archer "actor-archer" 8 2 3 1)
      (create-template :swordman "actor-swordman" 10 2 1 2)
      (create-template :berserk "actor-berserk" 8 3 1 2)
 
-     (create-template :rider "actor-rider" 12 3 1 3)
+     (create-template :rider "actor-rider" 12 3 1 4)
      (create-template :warlock "actor-warlock" 8 2 2 1)
      (create-template :skeleton "actor-skeleton" 8 2 1 2)
      (create-template :sniper "actor-sniper" 6 2 3 2)
      (create-template :werewolf "actor-werewolf" 18 3 1 2)]))
+
+;; get static template from actor entity
+(defn get-template [actor]
+  (get actors-template (keyword (get actor :template))))
